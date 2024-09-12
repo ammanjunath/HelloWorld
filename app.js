38cf364d24c1505bb9ba9842c819279b06289d69
ammanjunath <manjunathtek@gmail.com>
2024-09-12 14:56:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(bodyParser.jsn());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
