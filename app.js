1e0f694ed0f204df6861dbd2b5c012b3edef79a9
ammanjunath <manjunathtek@gmail.com>
2024-09-12 15:05:15 +0530

    Data updated
    data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -7,14 +8,14 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const mobiles=[
-  'Nokia',
-  'Realme',
-  'Redme'
-];
+var data = [{
+  name: 'Mobile update8',
+  id: '8',
+  amount: '80000'
+}];
 
 app.get('/plan', (req, res) => {
-  res.json('mobiles');
+  res.json(data);
 });
 
 app.listen(port, () => {
