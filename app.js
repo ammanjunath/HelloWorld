90e62ec7fe75ad04eb0485a06fc20734ad7224c9
ammanjunath <manjunathtek@gmail.com>
2024-09-12 14:55:53 +0530

    Update app.js
    APp update with get

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.jsn());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+const mobiles=[
+  'Nokia',
+  'Realme',
+  'Redme'
+];
+
+app.get('/plan', (req, res) => {
+  res.json('mobiles');
 });
 
 app.listen(port, () => {
